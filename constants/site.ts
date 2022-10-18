402eb3a90a333ad57430d370be78598d49c32394
Matteu <matteugt@gmail.com>
2022-10-18 16:38:44 +0800

    FEAT: Add site meta constants

---
+++ constants/site.ts
@@ -0,0 +1,9 @@
+export const site = {
+  'name': 'WebMasters Guild',
+  'tagline': 'An accredited organization under the School of IT of Mapúa University that specializes in Web development and graphics design.',
+  'version': '0.1.0',
+  'author': 'WebMasters Guild',
+  'url': 'https://wmgmapua.org',
+  'themeColor': '#2d3e50',
+  'twitter': '@wmgmapua'
+};
