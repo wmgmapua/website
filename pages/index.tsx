a42aaa26758d398f740388f8e6c3d955cf42c9ab
Matteu <matteugt@gmail.com>
2022-10-18 16:39:09 +0800

    FEAT: Add initial home page contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { NextPage } from 'next';
-import WMGButton from '../components/WMGButton';
+
+import DefaultLayout from '../layouts/DefaultLayout';
+import WMGButton from '../components/primitives/WMGButton';
+import WMGBanner from '../components/WMGBanner';
+
+import { site } from '../constants/site';
 
 const HomePage: NextPage = () => {
   return (
-    <WMGButton>
-      Hello World
-    </WMGButton>
+    <DefaultLayout title="HOME">
+      <WMGBanner title={site.name} subtitle="Forging bonds by building the web." media="mapua-intra-campus.webp" mediaAlt="Image of Mapua Intramuros Campus." alignment="left">
+        <WMGButton type="filled inverted" link="/about">Learn more</WMGButton>
+      </WMGBanner>
+      <main className="content-wrap">
+        <h2>Content</h2>
+        <p>Sample content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, eligendi perferendis? Aspernatur at aut consectetur eos illum magni officiis sint vel! Blanditiis, nobis, qui. Delectus minus odit provident quia quos!</p>
+      </main>
+    </DefaultLayout>
   );
 };
 
